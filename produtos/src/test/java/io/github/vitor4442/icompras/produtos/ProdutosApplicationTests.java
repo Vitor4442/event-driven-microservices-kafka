e18919aac3899b735262ea0407d3f3463f16558a
José Vitor <jose.vitorw018@gmail.com>
2026-08-14 00:58:09 -0300

    first commit

---
+++ produtos/src/test/java/io/github/vitor4442/icompras/produtos/ProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.vitor4442.icompras.produtos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
