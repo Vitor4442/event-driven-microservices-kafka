1abe6d00093630cbcb7f5cdde38338f5dd006519
José Vitor <jose.vitorw018@gmail.com>
2026-08-26 22:33:14 -0300

    service inical

--- pedidos/src/main/java/io/github/vitor4442/icompras/pedidos/service/PedidoService.java
+++ pedidos/src/main/java/io/github/vitor4442/icompras/pedidos/service/PedidoService.java
@@ -1,8 +1,11 @@
 package io.github.vitor4442.icompras.pedidos.service;
 
 
+import io.github.vitor4442.icompras.pedidos.model.Pedido;
 import io.github.vitor4442.icompras.pedidos.repository.ItemPedidoRepository;
 import io.github.vitor4442.icompras.pedidos.repository.PedidoRepository;
+import io.github.vitor4442.icompras.pedidos.validator.PedidoValidator;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -14,6 +17,12 @@ public class PedidoService {
 
     private final PedidoRepository repository;
     private final ItemPedidoRepository itemPedidoRepository;
+    private final PedidoValidator validator;
 
-    
+
+    @Transactional
+    public Pedido criarPedido(Pedido pedido){
+        validator.validar(pedido);
+        return pedido;
+    }
 }
---
+++ pedidos/src/main/java/io/github/vitor4442/icompras/pedidos/validator/PedidoValidator.java
@@ -0,0 +1,12 @@
+package io.github.vitor4442.icompras.pedidos.validator;
+
+import io.github.vitor4442.icompras.pedidos.model.Pedido;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PedidoValidator {
+
+    public void validar(Pedido pedido){
+
+    }
+}
