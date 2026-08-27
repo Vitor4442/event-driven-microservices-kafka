c4d9538b03fb7c8e1e5e6ca176365c95e27f7616
José Vitor <jose.vitorw018@gmail.com>
2026-08-26 22:32:37 -0300

    controller pedido

---
+++ pedidos/src/main/java/io/github/vitor4442/icompras/pedidos/controller/PedidoController.java
@@ -0,0 +1,25 @@
+package io.github.vitor4442.icompras.pedidos.controller;
+
+import io.github.vitor4442.icompras.pedidos.dto.NovoPedidoDTO;
+import io.github.vitor4442.icompras.pedidos.mapper.PedidoMapper;
+import io.github.vitor4442.icompras.pedidos.service.PedidoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("pedidos")
+@RequiredArgsConstructor
+public class PedidoController {
+
+    private final PedidoService service;
+    private final PedidoMapper mapper;
+
+    public ResponseEntity<Object> criar(@RequestBody NovoPedidoDTO dto){
+        var pedido = mapper.map(dto);
+        var novoPedido = service.criarPedido(pedido);
+        return ResponseEntity.ok(novoPedido.getCodigo());
+    }
+}
