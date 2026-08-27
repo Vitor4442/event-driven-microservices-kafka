adee51bf6ad5320a355a72045b2c69e39764f8a7
José Vitor <jose.vitorw018@gmail.com>
2026-08-26 23:44:15 -0300

    Criando o Client de micro servicos

---
+++ pedidos/src/main/java/io/github/vitor4442/icompras/pedidos/client/ProdutosClient.java
@@ -0,0 +1,13 @@
+package io.github.vitor4442.icompras.pedidos.client;
+
+import io.github.vitor4442.icompras.pedidos.client.representation.ProdutoRepresentation;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "produtos", url = "${icompras.pedidos.clients.produtos.url}")
+public interface ProdutosClient {
+    @GetMapping("{codigo}")
+    ResponseEntity<ProdutoRepresentation> obterDados(@PathVariable("codigo") Long codigo);
+}
--- pedidos/src/main/java/io/github/vitor4442/icompras/pedidos/validator/PedidoValidator.java
+++ pedidos/src/main/java/io/github/vitor4442/icompras/pedidos/validator/PedidoValidator.java
@@ -1,10 +1,19 @@
 package io.github.vitor4442.icompras.pedidos.validator;
 
+import io.github.vitor4442.icompras.pedidos.client.ProdutosClient;
+import io.github.vitor4442.icompras.pedidos.client.representation.ProdutoRepresentation;
 import io.github.vitor4442.icompras.pedidos.model.Pedido;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
+@RequiredArgsConstructor
 public class PedidoValidator {
+
+    private final ProdutosClient produtosClient;
 
     public void validar(Pedido pedido){
 
