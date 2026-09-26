e8ed28d2545afc3ac1f6001266126a82b7a70893
José Vitor <jose.vitorw018@gmail.com>
2026-09-26 00:25:34 -0300

    Ajustes

--- pedidos/src/main/java/io/github/vitor4442/icompras/pedidos/service/PedidoService.java
+++ pedidos/src/main/java/io/github/vitor4442/icompras/pedidos/service/PedidoService.java
@@ -1,8 +1,13 @@
 package io.github.vitor4442.icompras.pedidos.service;
 
 
+import io.github.vitor4442.icompras.pedidos.client.ClientesClient;
+import io.github.vitor4442.icompras.pedidos.client.ProdutosClient;
 import io.github.vitor4442.icompras.pedidos.client.ServicoBancarioClient;
+import io.github.vitor4442.icompras.pedidos.client.representation.ClienteRepresentation;
+import io.github.vitor4442.icompras.pedidos.client.representation.ProdutoRepresentation;
 import io.github.vitor4442.icompras.pedidos.model.DadosPagamento;
+import io.github.vitor4442.icompras.pedidos.model.ItemPedido;
 import io.github.vitor4442.icompras.pedidos.model.Pedido;
 import io.github.vitor4442.icompras.pedidos.model.enums.StatusPedido;
 import io.github.vitor4442.icompras.pedidos.model.enums.TipoPagamento;
@@ -13,8 +18,10 @@ import io.github.vitor4442.icompras.pedidos.validator.PedidoValidator;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -26,7 +33,8 @@ public class PedidoService {
     private final ItemPedidoRepository itemPedidoRepository;
     private final PedidoValidator validator;
     private final ServicoBancarioClient servicoBancarioClient;
-
+    private final ClientesClient apiClient;
+    private final ProdutosClient apiProdutos;
 
     @Transactional
     public Pedido criarPedido(Pedido pedido){
@@ -57,6 +65,7 @@ public class PedidoService {
 
         if(sucesso){
             pedido.setStatus(StatusPedido.PAGO);
+
         } else {
             pedido.setStatus(StatusPedido.ERRO_PAGAMENTO);
             pedido.setObservacoes(observacoes);
@@ -88,7 +97,33 @@ public class PedidoService {
 
         repository.save(pedido);
 
-
-
     }
+
+    public Optional<Pedido> carregarDadosCompletosPedido(Long codigo){
+        Optional<Pedido> pedido = repository.findById(codigo);
+        pedido.ifPresent(this::carregarDadosCliente);
+        pedido.ifPresent(this::carregarItensPedido);
+        return pedido;
+    }
+
+    private void carregarDadosCliente(Pedido pedido){
+        Long codigoCliente = pedido.getCodigoCliente();
+        ResponseEntity<ClienteRepresentation> response = apiClient.obterDados(codigoCliente);
+        pedido.setDadosCliente(response.getBody());
+    }
+
+    private void carregarItensPedido(Pedido pedido){
+        List<ItemPedido> itens = itemPedidoRepository.findByPedido(pedido);
+        pedido.setItens(itens);
+        pedido.getItens().forEach(this::carregarDadosProduto);
+    }
+
+    private void carregarDadosProduto(ItemPedido item){
+        Long codigoProduto = item.getCodigoProduto();
+        ResponseEntity<ProdutoRepresentation> response = apiProdutos.obterDados(codigoProduto);
+        item.setNome(response.getBody().nome());
+    }
+
+
+
 }
